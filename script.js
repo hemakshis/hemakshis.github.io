28c145d23cd3cdd3459ba7dc7bedabbac9f14ece
Hemakshi Sachdev <sachdev.hemakshi@gmail.com>
2018-08-18 22:53:06 +0530

    About page - [in progress]

--- script.js
+++ script.js
@@ -7,42 +7,47 @@
 $(window).on('scroll', function () {
     var scrollTop = $(this).scrollTop();
     if (scrollTop > 0) {
-      $("nav").css('background-color', 'rgba(0, 0, 0, 0.5)');
-      $("nav").find('a').css('color', 'white');
+        $("nav").css('background-color', 'rgba(0, 0, 0, 0.5)');
+        $("nav").find('a').css('color', 'white');
     } else {
-      $("nav").css('background-color', 'transparent');
-      $("nav").find('a').css('color', '#25272b');
+        $("nav").css('background-color', 'transparent');
+        $("nav").find('a').css('color', '#25272b');
     }
   });
   
-  $(document).ready(function () {
+$(document).ready(function () {
     'use strict';
 
-    const windowWidth = $(window).width();
+    const windowWidth = $(window).innerWidth;
     const windowHeight = $(window).height();
-
+    
     console.log(windowWidth, windowHeight);
 
-    $("#home").css('width', windowWidth);
-    $("#home").css('height', windowHeight);
+    $("#home, #about").css('width', windowWidth);
+    $("#home, #about").css('height', windowHeight);
 
     $('.carousel').carousel({
-      interval: false
+        interval: false
     });
 
     if ($(window).width() <= 768) {
-      $(".navbar-toggler").on('click', function () {
-        $(".navbar-brand").toggleClass('text-white');
-        $(".navbar-collapse").toggleClass('add-bgd');
-        $(".navbar-collapse").find('a').toggleClass('text-white');
-      });
+        $(".navbar-toggler").on('click', function () {
+            $(".navbar-brand").toggleClass('text-white');
+            $(".navbar-collapse").toggleClass('add-bgd');
+            $(".navbar-collapse").find('a').toggleClass('text-white');
+        });
     }
 
-
-  
     $("ul.navbar-nav").find('a').on('click', function () {
-      var $href = $(this).attr('href');
-      var $anchor = $($href).offset();
-      $('html,body').animate({ scrollTop: $anchor.top }, 'slow');
+        var $href = $(this).attr('href');
+        var $anchor = $($href).offset();
+        $('html,body').animate({ scrollTop: $anchor.top }, 'slow');
     });
-  });
+
+    $(".arrow").on('click', function() {
+        var $href = "#about";
+        var $anchor = $($href).offset();
+        $('html,body').animate({ scrollTop: $anchor.top }, 'slow');
+    });
+
+});
