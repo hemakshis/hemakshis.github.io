ba7f2aae0c7db3298b2b50758c73cb6b49bb177d
Hemakshi Sachdev <sachdev.hemakshi@gmail.com>
2018-10-22 12:34:06 +0530

    Projects Page Added

--- script.js
+++ script.js
@@ -19,17 +19,51 @@ $(window).on('scroll', function () {
 $(document).ready(function () {
     'use strict';
 
+    $("#api-dev").hide();
+    $("#front-end").hide();
+
+    $(".carousel").carousel({
+        interval: false
+    });
+
     const windowWidth = $(window).innerWidth;
     const windowHeight = $(window).height();
 
     console.log(windowWidth, windowHeight);
 
-    $("#home, #about").css('width', windowWidth);
-    $("#home, #about").css('height', windowHeight);
+    $("#home, #about, #projects").css('width', windowWidth);
+    $("#home, #about, #projects").css('height', windowHeight);
+
+    $("#about-container, #projects-container").css('height', windowHeight);
 
     $("ul.navbar-nav").find('a').on('click', function () {
         var $href = $(this).attr('href');
         var $anchor = $($href).offset();
         $('html,body').animate({ scrollTop: $anchor.top }, 'slow');
+    });
+
+    $("#a-full-stack").on('click', function() {
+        $("#full-stack").show();
+        $("#api-dev").hide();
+        $("#front-end").hide();
+    });
+
+    $("#a-api-dev").on('click', function() {
+        $("#full-stack").hide();
+        $("#api-dev").show();
+        $("#front-end").hide();
+    });
+
+    $("#a-front-end").on('click', function() {
+        $("#full-stack").hide();
+        $("#api-dev").hide();
+        $("#front-end").show();
+    });
+
+    $("#pagination-api-dev li a").click(function(e) {
+        $("#pagination-api-dev li.active").removeClass('active');
+        var $parent = $(this).parent();
+        $parent.addClass('active');
+        e.preventDefault();
     });
 });
